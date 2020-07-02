c8863a644385de07a6d91680d604a1475946248b
ziweiZhang <1146243152@qq.com>
2020-07-02 12:11:48 +0800

    fix(action): 异步请求根路径修改

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+const ROOT_PATH = process.env.NODE_ENV === 'production' ? '/douban-reader-swiper/build/' : '/'
 export const GET_LIST = 'GET_LIST';
 export const getList = (tag, list) => ({
     type: GET_LIST,
@@ -5,7 +6,7 @@ export const getList = (tag, list) => ({
     list
 });
 export const fetchList = tag => dispatch => {
-    return fetch(`/json/${tag}`, {headers: {'Content-Type': 'application/json; charset=utf-8'}})
+    return fetch(`${ROOT_PATH}json/${tag}`, {headers: {'Content-Type': 'application/json; charset=utf-8'}})
         .then(response => response.json())
         .then(json => dispatch(getList(tag, json.data)))
 }
